29eea9a2c9881cd3a18a7a7814a3aff22b8f7b3a
Ludvig <l.lundh@hotmail.com>
2023-05-29 15:23:57 +0200

    stuff

--- next.config.js
+++ next.config.js
@@ -8,23 +8,11 @@ const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
+    appDir: true,
     serverComponentsExternalPackages: ['prisma', '@prisma/client'],
   },
-  env: {
-    NEXT_EMAILJS_SERVICE_ID: process.env.NEXT_EMAILJS_SERVICE_ID,
-    NEXT_EMAILJS_TEMPLATE_ID: process.env.NEXT_EMAILJS_TEMPLATE_ID,
-    NEXT_EMAILJS_PUBLIC_KEY: process.env.NEXT_EMAILJS_PUBLIC_KEY,
-  },
+  env: {},
   i18n,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/dashboard',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/react-tailwind-datetime-picker/dist/index.esm.mjs',
-    './node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       transitionProperty: {
@@ -12,12 +8,6 @@ module.exports = {
         height: 'height',
       },
     },
-    colors: {
-      primary: '#1e2d3d',
-      primaryDark: '#141e29',
-      secondary: '#03b8d4',
-      secondaryDark: '#024954',
-    },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [],
 }
